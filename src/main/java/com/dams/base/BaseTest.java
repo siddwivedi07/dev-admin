b8c5060af56db6035bb912722010d0b6bd04d413
siddwivedi07 <07siddwivedi@gmail.com>
2026-03-24 11:34:04 +0530

    Fix import statement for BeforeMethod annotation

--- src/main/java/com/dams/base/BaseTest.java
+++ src/main/java/com/dams/base/BaseTest.java
@@ -4,7 +4,7 @@ import com.dams.driver.DriverFactory;
 import com.dams.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeMethod; 
 import org.testng.annotations.Listeners;
 
 @Listeners(com.dams.report.ExtentListener.class)
