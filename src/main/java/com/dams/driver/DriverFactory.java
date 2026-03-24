34c91df9aaf0d297872b2b28d413d6c8861e4022
siddwivedi07 <07siddwivedi@gmail.com>
2026-03-24 13:04:20 +0530

    Update DriverFactory.java

--- src/main/java/com/dams/driver/DriverFactory.java
+++ src/main/java/com/dams/driver/DriverFactory.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
-
+ 
 public class DriverFactory {
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
